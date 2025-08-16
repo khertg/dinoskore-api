fb683bda4d331914d03576fe01869d0ed5f1f9c1
khertg <khertg@gmail.com>
2025-08-16 12:24:29 +0800

    updates

--- DinoSkore.Api/Program.cs
+++ DinoSkore.Api/Program.cs
@@ -1,6 +1,7 @@
 using DinoSkore.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +11,23 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Read connection string from environment if on Railway, else use appsettings
+var databaseUrl = Environment.GetEnvironmentVariable("ConnectionStrings__Database")
+                  ?? builder.Configuration.GetConnectionString("Database");
+
+// Convert URL to Npgsql format and add SSL
+var npgsqlBuilder = new NpgsqlConnectionStringBuilder(databaseUrl)
+{
+    SslMode = SslMode.Require
+};
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options
-        .UseNpgsql(
-            builder.Configuration.GetConnectionString("Database"),
-            npgsqlOptions => npgsqlOptions
-                .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
-        .UseSnakeCaseNamingConvention());
+    options.UseNpgsql(
+        npgsqlBuilder.ToString(),
+        npgsqlOptions => npgsqlOptions
+            .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application)
+    ).UseSnakeCaseNamingConvention()
+);
 
 var app = builder.Build();
 
